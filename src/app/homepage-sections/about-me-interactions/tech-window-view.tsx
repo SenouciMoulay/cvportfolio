ba5bdef31d9a1167f902e99da2ea26753f1f6a5c
Dmitrii <kopenkin.da@gmail.com>
2024-11-06 14:18:06 +0100

    feat: integrate react-solid-flow for tech window view interactions and simplify rendering logic

--- src/app/homepage-sections/about-me-interactions/tech-window-view.tsx
+++ src/app/homepage-sections/about-me-interactions/tech-window-view.tsx
@@ -1,21 +1,19 @@
 "use client";
 
+import { Match, Switch } from "react-solid-flow";
 import { useTechWindow } from "./tech-window-context";
 
 export function TechWindowView() {
   const [window] = useTechWindow();
-  const showing = (() => {
-    switch (window) {
-      case null:
-        return <span>todo: tech carousel</span>;
-      default:
-        return <span>todo: implement {window} infographics</span>;
-    }
-  })();
+
   return (
     <div className="relative grid h-full w-full place-items-center">
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-      {showing}
+      <Switch fallback={<span>todo: implement {window} infographics</span>}>
+        <Match when={window === null}>
+          <span>todo: tech carousel</span>
+        </Match>
+      </Switch>
     </div>
   );
 }
